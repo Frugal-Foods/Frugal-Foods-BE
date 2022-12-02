8e051ad9ff14c8211ba9b35555d375304e9650f4
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-12-02 14:41:57 -0700

    store tests passing

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,8 @@
 class Store < ApplicationRecord
+  has_many :store_items
+  has_many :user_stores
+  has_many :items, through: :store_items
+  has_many :users, through: :user_stores
+  validates_presence_of :address
+  validates_presence_of :name
 end
