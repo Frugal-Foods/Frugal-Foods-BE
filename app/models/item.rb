8d9601c48fd3d1901f7989ab705b84490756cbf7
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-12-02 14:39:15 -0700

    item tests passing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  has_many :store_items
+  has_many :user_items
+  has_many :items, through: :user_items
+  has_many :stores, through: :store_items
+  validates :name, presence: true
 end
