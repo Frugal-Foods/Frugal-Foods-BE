d6c1b76f5d8d2177148ce4a6d1f19312ee9d0aa4
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-12-03 12:39:00 -0700

    store items endpoint available

--- app/graphql/types/store_item_type.rb
+++ app/graphql/types/store_item_type.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 module Types
   class StoreItemType < Types::BaseObject
     field :id, ID, null: false
-    field :store_id, Integer
-    field :item_id, Integer
+    field :store_id, Integer, null: false
+    field :item_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :price, Float
