19689e65786d2f3c2ae59028419bb4efa78acdd3
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-12-03 16:49:12 -0700

    add a completed method for store item names in the graphQL query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,7 +7,9 @@ module Types
     field :items, [Types::ItemType], null: true
     field :stores, [Types::StoreType], null: true
     field :store_items, [Types::StoreItemType], null: true
-    field :item_name, String, null: false
+    field :item_name, [Types::StoreItemType], null: true do
+      argument :store_item, String, required: true
+    end
 
     def item_name
       Item.first.name
@@ -27,6 +29,10 @@ module Types
 
     def store_items
       UserStoreItem.all
+    end
+
+    def item_name(store_item)
+      store_item.item_name
     end
 
 
