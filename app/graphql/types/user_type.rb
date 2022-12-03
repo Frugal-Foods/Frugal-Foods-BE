2ee26c3174f404872824f28c2aa87ee23d05810b
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-12-03 12:16:26 -0700

    user query done

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,9 +1,8 @@
-# frozen_string_literal: true
-
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :email, String
+    field :email, String, null: false
+    field :stores, [Types::StoreType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
