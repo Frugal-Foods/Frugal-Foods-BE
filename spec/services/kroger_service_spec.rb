fb59d0e2887c70633c906c637ba591683f9c9a6c
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-12-04 16:42:42 -0700

    Create KrogerService spec

---
+++ spec/services/kroger_service_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe KrogerService do
+  it 'returns Kroger grocery stores near a given zipcode' do
+    zipcode = '80108'
+    search = KrogerService.get_kroger_stores(zipcode)
+
+    expect(search).to be_a Hash
+    expect(search[:data]).to be_an Array
+
+    kroger_stores = search[:data]
+
+    kroger_stores.each do |store|
+      expect(store).to have_key :locationId
+      expect(store).to have_key :address
+      expect(store[:address]).to have_key :addressLine1
+      expect(store[:address]).to have_key :city
+      expect(store[:address]).to have_key :state
+      expect(store[:address]).to have_key :zipcode
+      expect(store[:address][:addressLine1]).to be_a String
+      expect(store[:address][:city]).to be_a String
+      expect(store[:address][:state]).to be_a String
+      expect(store[:address][:zipcode]).to be_a String
+      expect(store).to have_key :name
+    end
+  end
+end
