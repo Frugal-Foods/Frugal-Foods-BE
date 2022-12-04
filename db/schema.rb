88fdc713ce7ea186abae81046e1ab6c1ae77edd9
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-12-04 12:52:20 -0700

    Add Quantity attribute to UserStoreItems table and GraphQL type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_04_165628) do
+ActiveRecord::Schema.define(version: 2022_12_04_194951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_12_04_165628) do
     t.bigint "store_item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["store_item_id"], name: "index_user_store_items_on_store_item_id"
     t.index ["user_id"], name: "index_user_store_items_on_user_id"
   end
