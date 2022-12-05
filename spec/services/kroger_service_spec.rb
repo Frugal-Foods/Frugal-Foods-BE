1361ae6b27627ba1825f4711cd69312e89e9cf31
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-12-05 10:17:34 -0700

    Add test for get_items to KrogerService spec

--- spec/services/kroger_service_spec.rb
+++ spec/services/kroger_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe KrogerService do
-  it 'gets an authorization token before making the zipcode call', :vcr do
+  it 'can get an authorization token before making a call', :vcr do
     auth_token = KrogerService.authorize
     expect(auth_token).to be_an(Object)
   end
@@ -27,6 +27,34 @@ RSpec.describe KrogerService do
       expect(store[:address][:state]).to be_a String
       expect(store[:address][:zipCode]).to be_a String
       expect(store).to have_key :name
+    end
+  end
+
+  it 'can return grocery items from a specific store from a search', :vcr do
+    zipcode = '80108'
+    response = KrogerService.get_kroger_stores(zipcode)
+    store_id = response[:data][2][:locationId]
+    search_term = 'banana'
+
+    search = KrogerService.get_items(store_id: store_id, search_term: search_term)
+
+    expect(search).to be_a Hash
+    expect(search[:data]).to be_an Array
+
+    items = search[:data]
+
+    items.each do |item|
+      expect(item).to have_key :description
+      expect(item[:description]).to be_a String
+      expect(item).to have_key :images
+      expect(item[:images]).to be_an Array
+      expect(item[:images][0][:sizes][3][:url]).to be_a String
+      expect(item).to have_key :items
+      expect(item[:items]).to be_an Array
+      expect(items[0][:price][:regular]).to be_a Float
+      expect(items[0][:price][:promo]).to be_a Float
+      expect(items[0]).to have_key :size
+      expect(items[0][:size]).to be_a String
     end
   end
 end
