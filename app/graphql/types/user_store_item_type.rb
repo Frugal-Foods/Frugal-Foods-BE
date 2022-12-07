e72ae0d7f59708fd27c81f5d1b465b54d235d2b0
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-12-06 20:03:34 -0700

    Add name & price attribute to listItems

--- app/graphql/types/user_store_item_type.rb
+++ app/graphql/types/user_store_item_type.rb
@@ -7,7 +7,18 @@ module Types
     field :store_item_id, Integer
     field :quantity, Integer
     field :store, String
+    field :name, String
+    field :price, Float
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def name
+      store_item = StoreItem.find(object.store_item_id)
+      Item.find(store_item.item_id).name
+    end
+
+    def price
+      StoreItem.find(object.store_item_id).price
+    end
   end
 end
--- app/graphql/types/user_store_type.rb
+++ app/graphql/types/user_store_type.rb
@@ -6,11 +6,16 @@ module Types
     field :name, String
     field :address, String
     field :store_total_price, Float
+    field :list_items, [Types::UserStoreItemType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     def store_total_price
       object.total_price(object.store_id)
+    end
+
+    def list_items
+      object.user_store_items_by_store(object.store_id)
     end
   end
 end
