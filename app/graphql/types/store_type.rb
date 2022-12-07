35938f366fa9a2e5522f72f27de6ac6a187a1621
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-12-06 21:56:03 -0700

    Re-add store_items field to StoreType

--- app/graphql/types/store_type.rb
+++ app/graphql/types/store_type.rb
@@ -5,6 +5,7 @@ module Types
     field :address, String, null: false
     field :items, [Types::ItemType], null: true
     field :zipcode, String, null: false
+    field :store_items, [Types::StoreItemType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
