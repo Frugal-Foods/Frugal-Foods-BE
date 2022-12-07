7e6afe45a928e7340b8e39bed93b38fd0c2f6078
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-12-06 21:40:37 -0700

    Request spec for Get userStoreItems

--- spec/graphql/queries/get_user_store_item_spec.rb
+++ spec/graphql/queries/get_user_store_item_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Types::QueryType do
-  describe 'display UserStoreItems' do
+  describe 'Get UserStoreItems' do
     before :each do
       @user = User.create(email: Faker::Internet.email)
 
-      store1 = @user.stores.create(name: Faker::Commerce.vendor, address: Faker::Address.full_address)
-      store2 = @user.stores.create(name: Faker::Commerce.vendor, address: Faker::Address.full_address)
-      store3 = @user.stores.create(name: Faker::Commerce.vendor, address: Faker::Address.full_address)
+      store1 = @user.stores.create(name: Faker::Commerce.vendor, address: Faker::Address.full_address, zipcode: '12345')
+      store2 = @user.stores.create(name: Faker::Commerce.vendor, address: Faker::Address.full_address, zipcode: '12345')
+      store3 = @user.stores.create(name: Faker::Commerce.vendor, address: Faker::Address.full_address, zipcode: '12345')
+      store4 = Store.create(name: Faker::Commerce.vendor, address: Faker::Address.full_address, zipcode: '12345')
 
-      item1 = Item.create(name: Faker::Food.ingredient, photo_url: 'url.com')
-      item2 = Item.create(name: Faker::Food.ingredient, photo_url: 'url.com')
-      item3 = Item.create(name: Faker::Food.ingredient, photo_url: 'url.com')
+      item1 = Item.create(name: Faker::Food.ingredient, photo_url: Faker::Internet.url)
+      item2 = Item.create(name: Faker::Food.ingredient, photo_url: Faker::Internet.url)
+      item3 = Item.create(name: Faker::Food.ingredient, photo_url: Faker::Internet.url)
 
       storeitem1 = StoreItem.create(store_id: store1.id, item_id: item1.id, price: Faker::Commerce.price(range: 0..10.0))
       storeitem2 = StoreItem.create(store_id: store2.id, item_id: item1.id, price: Faker::Commerce.price(range: 0..10.0))
@@ -19,44 +20,69 @@ RSpec.describe Types::QueryType do
       storeitem4 = StoreItem.create(store_id: store2.id, item_id: item2.id, price: Faker::Commerce.price(range: 0..10.0))
       storeitem5 = StoreItem.create(store_id: store3.id, item_id: item3.id, price: Faker::Commerce.price(range: 0..10.0))
 
-      UserStoreItem.create(user_id: @user.id, store_item_id: storeitem1.id)
-      UserStoreItem.create(user_id: @user.id, store_item_id: storeitem4.id)
+      UserStoreItem.create(user_id: @user.id, store_item_id: storeitem1.id, quantity: 2)
+      UserStoreItem.create(user_id: @user.id, store_item_id: storeitem4.id, quantity: 5)
+      UserStoreItem.create(user_id: @user.id, store_item_id: storeitem2.id, quantity: 1)
 
-      post '/graphql', params: { query: query }
-      json = JSON.parse(response.body, symbolize_names: true)
-      binding.pry
-      data = json['data']['userStoreItems']
-
-      @result = FrugalFoodsBeSchema.execute(query).as_json
+      @result = JSON.parse(FrugalFoodsBeSchema.execute(query).to_json, symbolize_names: true)
     end
 
-    it 'can query all UserStoreItems' do
-      expect(response).to be_successful
-      expect(response[:data]).to be_a Hash
-      expect(@result['data']['userStoreItems'].count).to eq(2)
+    it 'can return all UserStoreItems, organized by store' do
+      expect(@result).to be_a Hash
+      expect(@result[:data][:userStoreItems]).to be_an Array
+      expect(@result[:data][:userStoreItems].count).to eq(3)
+
+      user_stores = @result[:data][:userStoreItems]
+
+      user_stores.each do |store|
+        expect(store).to have_key :storeId
+        expect(store).to have_key :name
+        expect(store).to have_key :address
+        expect(store).to have_key :storeTotalPrice
+        expect(store).to have_key :listItems
+        expect(store[:storeId]).to be_an Integer
+        expect(store[:name]).to be_a String
+        expect(store[:address]).to be_a String
+        expect(store[:storeTotalPrice]).to be_a Float
+        expect(store[:listItems]).to be_an Array
+
+        user_store_items = store[:listItems]
+
+        user_store_items&.each do |item|
+          expect(item).to have_key :id
+          expect(item).to have_key :itemName
+          expect(item).to have_key :itemPhotoUrl
+          expect(item).to have_key :price
+          expect(item).to have_key :quantity
+          expect(item).to have_key :itemTotal
+          expect(item[:id]).to be_a String
+          expect(item[:itemName]).to be_a String
+          expect(item[:itemPhotoUrl]).to be_a String
+          expect(item[:price]).to be_a Float
+          expect(item[:quantity]).to be_an Integer
+          expect(item[:itemTotal]).to be_a Float
+        end
+      end
     end
 
     def query
       <<~GQL
       {
         userStoreItems(userId: #{@user.id}) {
-          stores {
-            id
-            name
-            address
-            storeItems {
-              id
-              price
-              itemName
-              photoUrl
-              quantity
-              itemTotal
-            }
+          storeId
+          name
+          address
           storeTotalPrice
-          }
-        grandTotalPrice
-        }
-      }
+          listItems {
+                id
+                itemName
+                itemPhotoUrl
+                price
+                quantity
+                itemTotal
+              }
+         }
+    }
       GQL
     end
   end
