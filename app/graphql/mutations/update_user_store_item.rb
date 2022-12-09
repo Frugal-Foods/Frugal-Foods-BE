b55cd356ad0b5113981dc848c1070662adcdc97b
Carter Ball <carter.k.ball@gmail.com>
2022-12-09 15:56:05 -0500

    Remove unused code in UpdateUserStoreItem and app/graphql/types/store_item_input_type.rb

--- app/graphql/mutations/update_user_store_item.rb
+++ app/graphql/mutations/update_user_store_item.rb
@@ -1,33 +1,13 @@
 class Mutations::UpdateUserStoreItem < Mutations::BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
-    field :id, ID, null: false
-    #field :store_id, Types::StoreItemType, null: false
-    #field :store_name, Types::StoreType, null: false
-    field :quantity, Integer, null: false
-
+  field :id, ID, null: false
+  field :quantity, Integer, null: false
     
+  argument :id, ID, required: true    
+  argument :quantity, Integer, required: true
 
-    # # TODO: define arguments
-    #argument :name, String, required: true
-    argument :id, ID, required: true    
-    #argument :store_id, Types::StoreItemInputType, required: false
-    #argument :store_name, String#, required: true
-    argument :quantity, Integer, required: true
-
-    def resolve(attributes)
-      user_store_item = UserStoreItem.find_by(id: attributes[:id])
-      
-      user_store_item.update(quantity: attributes[:quantity])
-      user_store_item
-      #user_store_item.store_item.update(store_id: attributes[:store_id])
-      #binding.pry
-      # {
-      # user_store_item: user_store_item
-      # store_item: user_store_item.store_item
-      # }
-
-    end
-
-  
+  def resolve(attributes)
+    user_store_item = UserStoreItem.find_by(id: attributes[:id])
+    user_store_item.update(quantity: attributes[:quantity])
+    user_store_item
+  end
 end
--- app/graphql/types/store_item_input_type.rb
+++
@@ -1,9 +0,0 @@
-module Types
-  class StoreItemInputType < Types::BaseInputObject
-    argument :store_id, Integer, required: false
-    argument :item_id, Integer, required: false
-    argument :price, Float, required: false
-
-    
-  end
-end
